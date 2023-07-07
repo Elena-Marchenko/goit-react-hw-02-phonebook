8bfe29064a119377cba5f2ce98d42122dd3a81ab
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-07-07 23:33:27 +0300

    step 1 edits

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -47,6 +47,8 @@ class Form extends Component {
 
 export default Form;
 
+// pattern = '+?d{1,4}?[ .-s]?(?d{1,3}?)?[ .-s]?d{1,4}[ .-s]?d{1,4}[ .-s]?d{1,9}';
+
 // export default class Form extends Component {
 //   state = {
 //     name: '',
