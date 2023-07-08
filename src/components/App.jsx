2db0d70b3f23a2a2c527eb2f2ffe5c93a5678d7e
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-07-08 14:51:09 +0300

    step 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import Form from './form/Form';
-// import ContactForm from './form/Form';
+import Filter from './filter/Filter';
 import ContactList from './contactList/ContactList';
 // import './App.module.css';
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    filter: '',
   };
 
   formSubmitHandler = data => {
@@ -25,12 +26,28 @@ class App extends Component {
     }));
   };
 
+  onChangeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
+    const { filter, contacts } = this.state;
+    const normalaizFilter = filter.toLowerCase();
+
+    const visibleContacts = contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(normalaizFilter) ||
+        contact.number.toLowerCase().includes(normalaizFilter)
+    );
+
     return (
       <>
         <h1>Phonebook</h1>
         <Form onSubmit={this.formSubmitHandler} />
-        <ContactList items={this.state.contacts} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.onChangeFilter} />
+        <ContactList items={visibleContacts} />
       </>
     );
   }
