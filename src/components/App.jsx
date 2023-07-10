9edab93fdf98dca00733004138327d2af984dab1
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-07-10 19:27:08 +0300

    step 6
    убрала лишнее

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -62,36 +62,3 @@ class App extends Component {
   }
 }
 export default App;
-
-// export default class App extends Component {
-//   // model.id = nanoid();
-//   state = {
-//     contacts: [],
-//   };
-
-//   formSubmitData = data => {
-//     // console.log(data);
-//     const item = {
-//       id: nanoid(),
-//       ...data,
-//     };
-//     console.log(item);
-//   };
-
-//   render() {
-//     // const { contacts} = this.state;
-
-//     return (
-//       <>
-//         <div>
-//           <h1>Phonebook</h1>
-//           <ContactForm onSubmit={this.formSubmitData} />
-
-//           <h2>Contacts</h2>
-//           {/* <Filter /> */}
-//           {/* <ContactList contacts={data} /> */}
-//         </div>
-//       </>
-//     );
-//   }
-// }
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -11,9 +11,6 @@ class Form extends Component {
 
   handleChange = e => {
     const { name, value } = e.currentTarget;
-    // console.log(e.currentTarget);
-    // console.log(e.currentTarget.name.text);
-    // console.log(e.currentTarget.value);
 
     this.setState({
       [name]: value,
@@ -70,55 +67,3 @@ class Form extends Component {
 }
 
 export default Form;
-
-// pattern = '+?d{1,4}?[ .-s]?(?d{1,3}?)?[ .-s]?d{1,4}[ .-s]?d{1,4}[ .-s]?d{1,9}';
-
-// export default class Form extends Component {
-//   state = {
-//     name: '',
-//   };
-//   nameInputId = nanoid();
-
-//   handleChange = e => {
-//     const { name, value } = e.currentTarget;
-
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     // console.log(this.state);
-//     this.props.onSubmit(this.state);
-
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({
-//       name: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label htmlFor={this.nameInputId}>
-//           Name
-//           <input
-//             type="text"
-//             name="name"
-//             value={this.state.name}
-//             onChange={this.handleChange}
-//             id={this.nameInputId}
-//             // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//           />
-//         </label>
-//         <button type="submit">Add contact</button>
-//       </form>
-//     );
-//   }
-// }
